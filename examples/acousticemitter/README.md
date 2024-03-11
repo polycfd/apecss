dad002f71e5f6a38ffc1d9ba2a5bdfac685231f6
fabiandenner <fabian.denner@gmail.com>
2024-03-11 18:16:53 -0400

    Introduction of an acoustic emitter example

---
+++ examples/acousticemitter/README.md
@@ -0,0 +1,4 @@
+This example builds a standalone APECSS code for a simple acoustic emitter. 
+
+#### Spherical acoustic emitter
+Using [spherical.apecss](spherical.apecss) with ````./build/acousticemitter_apecss -options spherical.apecss -tend 4.0e-3 -fa 1500 -dpa 1```` runs a pulsating spherical acoustic emitter that produces a harmonic wave with a dimensionless wavenumber of $k_a R_0 = 1$ and a pressure amplitude of $\Delta p_\mathrm{a} = 1 \, \mathrm{Pa}$.
---
+++ examples/acousticemitter/src/acousticemitter_apecss.c
@@ -0,0 +1,159 @@
+// This source file is part of APECSS, an open-source software toolbox
+// for the computation of pressure-driven bubble dynamics and acoustic
+// emissions in spherical symmetry.
+//
+// Copyright (C) 2022-2024 The APECSS Developers
+//
+// The APECSS Developers are listed in the README.md file available in
+// the GitHub repository at https://github.com/polycfd/apecss.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// -------------------------------------------------------------------
+// APECSS standalone example for an acoustic emitter.
+// -------------------------------------------------------------------
+
+#include <time.h>
+#include "apecss.h"
+
+APECSS_FLOAT emitter_liquid_pressure_emitterwall(APECSS_FLOAT *Sol, APECSS_FLOAT t, struct APECSS_Bubble *Bubble);
+
+APECSS_FLOAT fa = 0.0;  // Acoustic frequency
+APECSS_FLOAT dpa = 0.0;  // Acoustic pressure amplitude
+
+int main(int argc, char **args)
+{
+  char str[APECSS_STRINGLENGTH_SPRINTF];
+  char OptionsDir[APECSS_STRINGLENGTH];
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // Initialize the case-dependent simulation parameters
+  APECSS_FLOAT tend = 0.0;  // End time of the simulation
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  apecss_infoscreen();
+
+  /* Read commandline options */
+  sprintf(OptionsDir, "./run.apecss");  // This is the default
+  int i = 1;  // First argument is the call to the executable
+  while (i < argc)
+  {
+    if (strcmp("-options", args[i]) == 0)
+    {
+      sprintf(OptionsDir, "%s", args[i + 1]);
+      i += 2;
+    }
+    else if (strcmp("-fa", args[i]) == 0)
+    {
+      sscanf(args[i + 1], "%le", &fa);
+      i += 2;
+    }
+    else if (strcmp("-dpa", args[i]) == 0)
+    {
+      sscanf(args[i + 1], "%le", &dpa);
+      i += 2;
+    }
+    else if (strcmp("-tend", args[i]) == 0)
+    {
+      sscanf(args[i + 1], "%le", &tend);
+      i += 2;
+    }
+    else if (strcmp("-h", args[i]) == 0)
+    {
+      apecss_helpscreen();
+    }
+    else
+    {
+      char str[APECSS_STRINGLENGTH_SPRINTF];
+      sprintf(str, "Unknown command line options: %s", args[i]);
+      apecss_erroronscreen(1, str);
+      ++i;
+    }
+  }
+
+  // Desired time step (1000 time steps per period)
+  APECSS_FLOAT dt_desired = 1.0 / (1000.0 * fa);
+
+  /* Allocate and initialize Bubble structure */
+  struct APECSS_Bubble *Bubble = (struct APECSS_Bubble *) malloc(sizeof(struct APECSS_Bubble));
+  apecss_bubble_initializestruct(Bubble);
+  apecss_bubble_setdefaultoptions(Bubble);
+  apecss_bubble_readoptions(Bubble, OptionsDir);
+
+  struct APECSS_Liquid *Liquid = (struct APECSS_Liquid *) malloc(sizeof(struct APECSS_Liquid));
+  apecss_liquid_setdefaultoptions(Liquid);
+  apecss_liquid_readoptions(Liquid, OptionsDir);
+  Bubble->Liquid = Liquid;
+
+  apecss_bubble_processoptions(Bubble);
+  apecss_liquid_processoptions(Liquid);
+
+  Bubble->Liquid->get_pressure_bubblewall = emitter_liquid_pressure_emitterwall;
+
+  Bubble->t = Bubble->tStart;
+  Bubble->dt = dt_desired;
+  Bubble->R = Bubble->R0;
+  Bubble->U = Bubble->U0;
+
+  Bubble->dtNumber = 0;
+  Bubble->emissions_initialize(Bubble);
+
+  clock_t starttimebubble = clock();
+
+  // Solve the acoustic emissions
+  while (Bubble->t < tend - APECSS_SMALL)
+  {
+    APECSS_FLOAT next_event_time = apecss_results_emissionstime_checktime(tend, Bubble);
+    Bubble->dt = APECSS_MIN(dt_desired, next_event_time - Bubble->t);
+
+    ++(Bubble->dtNumber);
+    Bubble->t += Bubble->dt;
+
+    APECSS_FLOAT p = Liquid->get_pressure_bubblewall(Bubble->ODEsSol, Bubble->t, Bubble);
+    APECSS_FLOAT rho = Liquid->get_density(p, Liquid);
+    APECSS_FLOAT c = Liquid->get_soundspeed(p, rho, Liquid);
+
+    APECSS_FLOAT omega = 2.0 * APECSS_PI * fa;
+    Bubble->U = dpa * APECSS_COS(omega * Bubble->t - 0.5 * APECSS_PI) / (rho * c);
+    Bubble->R = Bubble->R0 - dpa * APECSS_SIN(omega * Bubble->t - 0.5 * APECSS_PI) / ((omega + APECSS_SMALL) * rho * c);
+
+    // Update allocation of the results (if necessary)
+    Bubble->results_emissionsnodeminmax_identify(Bubble);
+    Bubble->results_emissionsnode_alloc(Bubble);
+
+    // Acoustic emissions (if applicable)
+    Bubble->emissions_update(Bubble);
+
+    // Store results (if applicable)
+    Bubble->results_emissionsspace_store(Bubble);
+
+    // Write one-off results (if applicable)
+    Bubble->results_emissionstime_write(Bubble);
+  }
+
+  apecss_bubble_solver_finalize(Bubble);
+
+  sprintf(str, "Solver concluded %i time-steps in %.3f s.", Bubble->dtNumber, (double) (clock() - starttimebubble) / CLOCKS_PER_SEC);
+  apecss_writeonscreen(str);
+
+  /* Write out all desired results */
+  apecss_results_emissionsspace_write(Bubble, APECSS_RESULTS_WRITE);
+  apecss_results_emissionsnodespecific_write(Bubble);
+  apecss_results_emissionsnodeminmax_write(Bubble);
+
+  /* Make sure all allocated memory is freed */
+  apecss_bubble_freestruct(Bubble);
+
+  free(Bubble);
+  free(Liquid);
+
+  return (0);
+}
+
+APECSS_FLOAT emitter_liquid_pressure_emitterwall(APECSS_FLOAT *Sol, APECSS_FLOAT t, struct APECSS_Bubble *Bubble)
+{
+  return (Bubble->p0 + 0.5 * dpa * APECSS_COS(2.0 * APECSS_PI * fa * Bubble->t - 0.5 * APECSS_PI));
+  // return (Bubble->p0 + 0.5 * dpa * (1.0 - APECSS_COS(2.0 * APECSS_PI * fa * Bubble->t)));
+}
