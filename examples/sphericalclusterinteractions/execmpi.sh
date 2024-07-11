7ee9c2552debb785fbd5e3a6d110c47d1dfa8481
Pierre C <pierre.coulombel@polymtl.ca>
2024-07-11 16:47:49 -0400

    spherical cluster interactions ; new test case similar to Maeda 2019 work

---
+++ examples/sphericalclusterinteractions/execmpi.sh
@@ -0,0 +1,9 @@
+cd build
+./compile.sh
+cd ..
+mpiexec -n 15 ./build/sphericalclusterinteractions_apecss -options run.apecss -freq 50.0e03 -amp -2.5e5 -tend 5.0e-07
+
+for ((c=0; c<2500; c++))
+do
+    rm -rf Bubble_$c
+done
