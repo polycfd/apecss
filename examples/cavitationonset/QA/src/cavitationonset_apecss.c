646186647d95905f217da7d278d4b6af0e76acda
Pierre C <pierre.coulombel@polymtl.ca>
2024-09-10 17:08:10 -0400

    cavitationonset ; retrieving more date for post-processing with QA computations

--- examples/cavitationonset/QA/src/cavitationonset_apecss.c
+++ examples/cavitationonset/QA/src/cavitationonset_apecss.c
@@ -383,6 +383,18 @@ int main(int argc, char **args)
     {
       fprintf(file_ida2009, " %e", Bubbles[i]->get_pressure_infinity(tSim, Bubbles[i]));
     }
+    for (register int i = 0; i < nBubbles; i++)
+    {
+      fprintf(file_ida2009, " %e", Bubbles[i]->ode[0](Bubbles[i]->ODEsSol, Bubbles[i]->t, Bubbles[i]));
+    }
+    for (register int i = 0; i < nBubbles; i++)
+    {
+      fprintf(file_ida2009, " %e", Bubbles[i]->Interaction->dp_neighbor);
+    }
+    for (register int i = 0; i < nBubbles; i++)
+    {
+      fprintf(file_ida2009, " %e", Bubbles[i]->U);
+    }
     fprintf(file_ida2009, "\n");
     // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
