d2eafa33c80c865025186327a364dd4ac8cf4989
Pierre C <pierre.coulombel@polymtl.ca>
2024-09-20 15:16:02 -0400

    cavitationonset ; adding two lines in gather_results.py in order to create the results folder if it does not exist already

--- examples/cavitationonset/IC/gather_results.py
+++ examples/cavitationonset/IC/gather_results.py
@@ -14,6 +14,8 @@ file_name = "{}_{:.4E}_{:.2f}.txt".format(count, png, cl_size)
 
 working_path = os.getcwd()
 results_path = os.path.join(working_path, "results")
+if not os.path.exists(results_path) :
+    os.makedirs(results_path)
 file_results = open(os.path.join(results_path, file_name), "w")
 
 for line in lines :
